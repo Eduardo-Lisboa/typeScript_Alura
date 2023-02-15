071e913c5519cb3c29696cb30cf8f8f1b8741647
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2023-02-14 22:42:05 -0300

    atualizaçoes na tipagem e novos metodos

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,7 +1,7 @@
 export class Negociacao {
     constructor(data, quantidade, valor) {
         this._data = data;
-        this._data = quantidade;
+        this._quantidade = quantidade;
         this._valor = valor;
     }
     get data() {
